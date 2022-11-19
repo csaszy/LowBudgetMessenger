9506756192f3790b9209d426e3339e434493d151
csaszy <csaszy2006@gmail.com>
2022-11-19 18:14:25 +0100

    better message forwarding to clients

--- server.py
+++ server.py
@@ -13,28 +13,27 @@ server.bind(ADDR)
 
 class Client:
     def __init__(self, id, conn, addr) -> None:
-        print('obj created')
+        #print('obj created')
         self.conn = conn
         self.addr = addr
         self.id = id
         self.connected = True
         #self.thread = threading.Thread(target=self.Recv) 
         #self.thread.start()
-    def Send(self, msg):
-        msg_length = len(msg)
-        send_length = str(msg_length).encode(FORMAT)
-        send_length += b" " * (HEADER-len(send_length)) #make it 64 bytes
+    def Send(self, msg, send_length):
+       #msg_length = len(msg)
+       #send_length = str(msg_length).encode(FORMAT)
+       #send_length += b" " * (HEADER-len(send_length)) #make it 64 bytes
         self.conn.sendto(send_length,(self.addr[0],self.addr[1]))
-        self.conn.sendto(msg.encode(FORMAT),(self.addr[0],self.addr[1]))
+        self.conn.sendto(msg,(self.addr[0],self.addr[1]))
     def Recv(self):
-        print(f'{self.id} receiving')
-        msg_length = self.conn.recv(HEADER).decode(FORMAT) #blocking code --> block the flow until it receves a message
+        #print(f'{self.id} receiving')
+        msg_length = self.conn.recv(HEADER) #blocking code --> block the flow until it receves a message
         if msg_length:
-            msg_length = int(msg_length)
-            msg = self.conn.recv(msg_length).decode(FORMAT)
+            msg = self.conn.recv(int(msg_length))#.decode(FORMAT)
             if msg == DISCONNENCT_MSG:
                 self.connected = False
-        return msg
+        return msg, msg_length
     def Get(self):
         return self.id,self.conn,self.addr
 
@@ -44,11 +43,11 @@ def handleClient(client: Client): #conn = connection; addr = address
     id, conn, addr = client.Get()
     print(f"[NEW_CONNECTION] --> {addr} connected")
     while True:
-        msg = client.Recv()
-        print(f"[{client.Get()[-1]}]: {msg}")
+        rawMsg, length = client.Recv()
+        print(f"[{client.Get()[-1]}]: {rawMsg.decode(FORMAT)}")
         for c in clients:
             if c != client:
-                c.Send(msg)
+                c.Send(rawMsg,length)
     #connected = True
     #while connected:
     #    msg_length = conn.recv(HEADER).decode(FORMAT) #blocking code --> block the flow until it receves a message
